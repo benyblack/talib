4fd17dffe65fff2a69c25bac1f943504d02537bc
Behnam Shomali <behnam.shomalis@tretton37.com>
2019-04-01 16:12:39 +0200

    Update version

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule TAlib.MixProject do
   use Mix.Project
 
   @app_name :talib
-  @version "0.3.0"
+  @version "0.3.1"
   @elixir_version "~> 1.8"
   @github "https://github.com/benyblack/talib"
 
